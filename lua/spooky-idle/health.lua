5370a8fc033f9d91a14e06299c562571e05c0f93
Akshar Patel <aksharcommit@gmail.com>
2025-10-28 22:01:36 -0400

    add health check

--- lua/spooky-idle/health.lua
+++ lua/spooky-idle/health.lua
@@ -1,1 +1,14 @@
--- TODO
+local health = vim.health
+local M = {}
+
+function M.check()
+	health.report_start("spooky-idle.nvim")
+	local ok = pcall(require, "spooky-idle.config")
+	if ok then
+		health.report_ok("Config OK")
+	else
+		health.report_error("Config failed")
+	end
+end
+
+return M

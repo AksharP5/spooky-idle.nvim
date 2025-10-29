ed9e8046c9460976f06332a67dfbaf9020133c6d
Akshar Patel <aksharcommit@gmail.com>
2025-10-28 21:57:52 -0400

    core.lua with subcommands

--- lua/spooky-idle/core.lua
+++ lua/spooky-idle/core.lua
@@ -1,1 +1,16 @@
--- TODO
+local M = {}
+local cfg = require("spooky-idle.config").get()
+
+function M.start()
+	vim.notify("spooky-idle haunting started for " .. cfg.idle_time .. "ms")
+end
+
+function M.stop()
+	vim.notify("spooky-idle stopped")
+end
+
+function M.toggle()
+	vim.notify("spooky-idle toggled")
+end
+
+return M
